240937e80cb2cc730ecac3bbd2456ed785c1d9eb
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-22 15:12:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,9 @@ def using_push(array, string)
   new_array.push(string)
   return new_array
 end
+
+def using_unshift(array, string)
+  new_array = []
+  new_array.push(string)
+  return new_array
+end
